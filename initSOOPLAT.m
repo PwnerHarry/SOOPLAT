af4b97494b6e8fae02e2240f690a487131167945
Harry Zhao <mingdeharry@outlook.com>
2018-11-09 23:05:53 -0500

    Update initSOOPLAT.m

--- initSOOPLAT.m
+++ initSOOPLAT.m
@@ -1,12 +1,10 @@
 function initSOOPLAT()
-    % Please do not use "clear functions" anywhere else
-    % for the SOOPLAT relies on the persistent variables
-    rng('shuffle');
-    main_path = fileparts(mfilename('fullpath'));
-    cd(main_path);
-    addpath(genpath(fullfile(main_path, 'Algorithms')));
-    addpath(genpath(fullfile(main_path, 'Public')));
-    addpath(genpath(fullfile(main_path, 'Benchmarks')));
-    addpath(genpath(fullfile(main_path, 'Accessories')));
-    addpath(genpath(fullfile(main_path, 'GUI')));
+rng('shuffle');
+main_path = fileparts(mfilename('fullpath'));
+cd(main_path);
+addpath(genpath(fullfile(main_path, 'Algorithms')));
+addpath(genpath(fullfile(main_path, 'Public')));
+addpath(genpath(fullfile(main_path, 'Benchmarks')));
+addpath(genpath(fullfile(main_path, 'Accessories')));
+addpath(genpath(fullfile(main_path, 'GUI')));
 end

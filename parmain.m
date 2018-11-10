d3ef2f033c2172dc47f41e07b89a6485b8afdcae
Harry Zhao <mingdeharry@outlook.com>
2018-11-09 23:43:36 -0500

    Update parmain.m

--- parmain.m
+++ parmain.m
@@ -1,23 +1,22 @@
 function parmain()
-    %%%%%%%%%%%%%%%%%%%
-    algorithms = {'CSO', 'FRG'};
-    function_index = 1;
-    runtimes = 1;
-    %%%%%%%%%%%%%%%%%
-    for R = 1: runtimes
-        for i = function_index
-            for j = 1: numel(algorithms)
-                % Global(i, j) = GLOBAL([], 5e6, suitestr, i, algorithms{j}, parameters{j});
-                Global(i, j) = GLOBAL('-algorithm', algorithms{j}, '-evaluation', 5e6, '-dimension', 1000, '-problem', sprintf('LSO08F%d', i));
-            end
-        end
-        title(sprintf("Run %d of %d", R, runtimes));
-        drawnow;
-        for i = function_index
-            parfor j = 1: numel(algorithms)
-                feval(algorithms{j}, Global(i, j));
-                Global(i, j).draw();
-            end
+% This script is for parallel running
+algorithms = {'CSO', 'FRG'};
+function_index = 1;
+runtimes = 1;
+for R = 1: runtimes
+    for i = function_index
+        for j = 1: numel(algorithms)
+            % Global(i, j) = GLOBAL([], 5e6, suitestr, i, algorithms{j}, parameters{j});
+            Global(i, j) = GLOBAL('-algorithm', algorithms{j}, '-evaluation', 5e6, '-dimension', 1000, '-problem', sprintf('LSO08F%d', i));
         end
     end
+    title(sprintf("Run %d of %d", R, runtimes));
+    drawnow;
+    for i = function_index
+        parfor j = 1: numel(algorithms)
+            feval(algorithms{j}, Global(i, j));
+            Global(i, j).draw();
+        end
+    end
+end
 end
